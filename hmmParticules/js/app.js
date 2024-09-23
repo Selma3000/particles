756775d40d7a4effdf77ce9037cf465d2b5754ff
Aissa <selma.aissa@etu.univ-nantes.fr>
2024-09-23 13:17:16 +0200

    setup class particles and start to animate

--- hmmParticules/js/app.js
+++ hmmParticules/js/app.js
@@ -1,15 +1,67 @@
 const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.fillStyle = 'white';
+
+console.log(context);
 
 
 
 class Particle {
+    constructor(effect) {
+      this.effect = effect;
+      this.radius = 20 * Math.random();
 
+      this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
+      this.y = this.radius + Math.random() * (this.effect.height -this.radius * 2);
+    }
+    // run 60 times per second
+    draw(context){
+      context.fillStyle = 'hsl('+ Math.random() * 230+',70%,36%)'
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius,0,Math.PI*2);
+      context.fill();
+      context.stroke();
+    }
+    update() {
+      this.x++;
+      this.y++;
+      if (this.x > canvas.width) {
+      }
+    }
 }
 
 class Effect {
-
+    constructor(canvas) {
+      this.canvas = canvas ;
+      this.width= this.canvas.width;
+      this.height = this.canvas.height
+      this.particles = [];
+      this.numberOfParticles = 200;
+      this.createParticles();
+    }
+    createParticles() {
+      for (let i = 0; i <this.numberOfParticles; i++) {
+        // array push method takes what we pass to it and
+        // pushed that to the end of the array
+        this.particles.push(new Particle(this))    }
+    }
+    handleParticles(context) {
+      this.particles.forEach(particle => {
+          particle.draw(context)
+        particle.update()
+        }
+      )
+    }
 }
+const effect = new Effect(canvas);
+
 
 function animate() {
-
+  context.clearRect(0,0,canvas.width,canvas.height)
+  effect.handleParticles(context)
+  requestAnimationFrame(animate)
 }
+
+animate()
